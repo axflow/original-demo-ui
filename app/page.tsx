9c032d12297e452754d05a7c924f15c6bf775ea6
Nicholas Charriere <nicholascharriere@gmail.com>
2023-07-17 17:24:29 -0700

    Add query tab content

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useState } from 'react';
 import { Separator } from '@/components/ui/separator';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -14,6 +15,7 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
 import { toast } from '@/components/ui/use-toast';
@@ -101,22 +103,44 @@ function IngestDocumentUpload() {
     });
   };
 
+  const validateFile = (file: FileList) => {
+    if (file.length !== 1) {
+      return false;
+    }
+
+    const filename = file[0].name;
+    if (!filename.endsWith('.md') && !filename.endsWith('.mdx')) {
+      return false;
+    }
+    return true;
+  };
+
   return (
     <form onSubmit={handleSubmit(onUpload)}>
-      <div className="mt-4 flex items-center gap-2">
-        <Label className="min-w-fit" htmlFor="document">
-          Upload document
-        </Label>
-        <Input
-          {...register('document', { required: true })}
-          type="file"
-          className="file:rounded file:bg-input hover:file:cursor-pointer file:hover:bg-accent"
-        />
-        {errors.document && <p className="text-sm text-destructive">This field is required</p>}
+      <div className="flex flex-col">
+        <div className="mt-4 flex items-center gap-2">
+          <Label className="min-w-fit" htmlFor="document">
+            Upload document
+          </Label>
+          <Input
+            {...register('document', { required: true, validate: validateFile })}
+            type="file"
+            className="file:rounded file:bg-input hover:file:cursor-pointer file:hover:bg-accent"
+          />
+        </div>
+        <p className="mt-2 text-sm text-muted-foreground">
+          We currently only support markdown files (.md)
+        </p>
+        {errors.document?.type === 'required' && (
+          <p className="text-sm text-destructive">This field is required</p>
+        )}
+        {errors.document?.type === 'validate' && (
+          <p className="text-sm text-destructive">Please upload a markdown file</p>
+        )}
       </div>
 
       <div className="mt-4 flex items-center justify-center">
-        <Button type="submit" className="mt-4">
+        <Button type="submit" className="mt-4" disabled={!!errors.document}>
           Upload
         </Button>
       </div>
@@ -151,28 +175,108 @@ function IngestTab() {
 }
 
 function QueryTab() {
+  const [response, setResponse] = useState<string>('');
+  const QuerySchema = z.object({
+    prompt: z
+      .string({
+        required_error: 'Please write your question',
+      })
+      .min(10, { message: 'The minimum prompt length is 10 characters.' })
+      .max(3000, { message: 'The maximum prompt length is 3000 characters.' }),
+    filter: z.string().optional(),
+  });
+  const queryForm = useForm<z.infer<typeof QuerySchema>>({
+    resolver: zodResolver(QuerySchema),
+  });
+  function onSubmit(data: z.infer<typeof QuerySchema>) {
+    toast({
+      title: 'Query',
+      description: <p>{data.prompt}</p>,
+    });
+    setResponse('The response would appear here...');
+  }
   return (
-    <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-      Ask your data questions
-    </h1>
+    <section className="my-8 flex w-full flex-col items-center">
+      <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+        Ask a question
+      </h1>
+      <div className="w-3/4">
+        <Form {...queryForm}>
+          <form onSubmit={queryForm.handleSubmit(onSubmit)}>
+            <FormField
+              control={queryForm.control}
+              name="prompt"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="mt-4 flex flex-col gap-2">
+                    <FormLabel className="min-w-fit">Question</FormLabel>
+                    <FormControl>
+                      <Textarea placeholder="Ask your data a question" {...field} />
+                    </FormControl>
+                  </div>
+                  <FormDescription>
+                    Ask a question, the data uploaded will be used to answer.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={queryForm.control}
+              name="filter"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="mt-4 flex flex-col gap-2">
+                    <FormLabel className="min-w-fit">Filter term</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        placeholder="Filter a document name (like 'San Francisco')"
+                        {...field}
+                      />
+                    </FormControl>
+                  </div>
+                  <FormDescription>
+                    Only fetch documents with a specific wikipedia term.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="mt-4 flex items-center justify-center">
+              <Button type="submit" className="mt-4">
+                Query
+              </Button>
+            </div>
+          </form>
+        </Form>
+
+        <Label>Response</Label>
+        <Textarea placeholder="" value={response} />
+      </div>
+    </section>
   );
 }
 export default function IndexPage() {
   return (
     <section className="flex w-full flex-col items-center gap-6">
-      <Tabs defaultValue="ingest" className="m-4">
+      <Tabs defaultValue="ingest" className="m-4 w-full">
         <div className="flex items-center justify-center">
           <TabsList>
             <TabsTrigger value="ingest">Ingest documents</TabsTrigger>
             <TabsTrigger value="query">Query</TabsTrigger>
           </TabsList>
         </div>
-        <TabsContent value="ingest">
-          <IngestTab />
-        </TabsContent>
-        <TabsContent value="query">
-          <QueryTab />
-        </TabsContent>
+        <div className="w-full">
+          <TabsContent value="ingest">
+            <IngestTab />
+          </TabsContent>
+          <TabsContent value="query">
+            <QueryTab />
+          </TabsContent>
+        </div>
       </Tabs>
     </section>
   );
