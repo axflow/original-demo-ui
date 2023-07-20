317eb031b605db6ed09937b3c8ee2441adf9e15a
Nicholas Charriere <nicholascharriere@gmail.com>
2023-07-19 18:20:45 -0700

    Pass temperature through to openAI call

--- lib/query.ts
+++ lib/query.ts
@@ -11,27 +11,40 @@ import {
 } from 'axgen';
 import { getPineconeStore, getOpenAiKey } from './axgen-utils';
 
-export async function query(question: string, model: string, llmOnly: boolean, topK: number) {
-  return llmOnly ? completion(question, model) : rag(question, model, topK);
+type QueryOptions = {
+  query: string;
+  model: string;
+  llmOnly: boolean;
+  topK: number;
+  temperature: number;
+  // TODO advanced filtering with DSL supporting operators $and, $or, $lt, $gt, $eq, $neq, $in, $nin
+  // For now, hardcode to matching the 'term' field of the metadata
+  filterTerm?: string;
+};
+export async function query(opts: QueryOptions) {
+  return opts.llmOnly
+    ? completion(opts.query, opts.model, opts.temperature)
+    : rag(opts.query, opts.model, opts.topK, opts.temperature);
 }
 
-export const completion = async (query: string, model: string) => {
+export const completion = async (query: string, model: string, temperature: number) => {
   const completion = new Completion({
-    model: new OpenAICompletion({ model: model, max_tokens: 256 }),
+    model: new OpenAICompletion({ model: model, max_tokens: 256, temperature }),
     prompt: new BasicPrompt({ template: QUESTION_WITHOUT_CONTEXT }),
   });
 
   return completion.run(query);
 };
 
-export const rag = async (question: string, model: string, topK: number) => {
+export const rag = async (question: string, model: string, topK: number, temperature: number) => {
   const pinecone = getPineconeStore();
 
   const rag = new RAG({
     model: new OpenAICompletion({
-      model: model,
+      model,
       max_tokens: 256,
       apiKey: getOpenAiKey(),
+      temperature,
     }),
     prompt: new PromptWithContext({ template: QUESTION_WITH_CONTEXT }),
     embedder: new OpenAIEmbedder({ apiKey: getOpenAiKey() }),
