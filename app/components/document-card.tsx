9183e1ce5c35502457ff95d61c6ee3a4742db148
Nicholas Charriere <nicholascharriere@gmail.com>
2023-07-22 16:50:03 -0700

    Cleanup the CSS for the document cards

--- app/components/document-card.tsx
+++ app/components/document-card.tsx
@@ -1,5 +1,13 @@
 import type { ContextDocument } from '@/lib/types';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 
 export default function DocumentCard({
   document,
@@ -9,19 +17,35 @@ export default function DocumentCard({
   idx: number;
 }) {
   return (
-    <>
-      <Card className="hover:bg-border">
-        <CardHeader>
-          <CardTitle>Document chunk #{idx + 1}</CardTitle>
-        </CardHeader>
-        <CardContent className="text-sm">{document.chunk.text}</CardContent>
-        {document.similarity && (
-          <CardFooter className="flex gap-2">
-            <p>similarity:</p>
-            <pre className="text-sm">{document.similarity}</pre>
-          </CardFooter>
-        )}
-      </Card>
-    </>
+    <Dialog>
+      <DialogTrigger className="h-full">
+        <Card className="flex min-h-full flex-col items-start hover:bg-border">
+          <CardHeader>
+            <CardTitle>Chunk #{idx + 1}</CardTitle>
+          </CardHeader>
+          <CardContent className="text-left text-sm">
+            {document.chunk.text.slice(0, 150)}...
+          </CardContent>
+          {document.similarity && (
+            <CardFooter className="flex gap-2">
+              <p>similarity:</p>
+              <pre className="text-xs">{document.similarity}</pre>
+            </CardFooter>
+          )}
+        </Card>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Chunk #{idx + 1}</DialogTitle>
+          <DialogDescription>
+            Source:{' '}
+            <a className="underline" href={document.chunk.url}>
+              {document.chunk.url}
+            </a>
+          </DialogDescription>
+        </DialogHeader>
+        <div>{document.chunk.text}</div>
+      </DialogContent>
+    </Dialog>
   );
 }
